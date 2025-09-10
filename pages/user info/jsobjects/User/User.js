5e4a653f0ced5ed918ea63e82c600b97898dc6c2
gcpop-charleshsu <168400762+gcpop-charleshsu@users.noreply.github.com>
2025-09-10 22:28:26 +0800

    Feat/create user (#15)

--- pages/user info/jsobjects/User/User.js
+++ pages/user info/jsobjects/User/User.js
@@ -1,14 +1,30 @@
 export default {
-	 showDetail: () => {
-		  storeValue('selectedUser', TableUserInfo.selectedRow)
-			showModal('UD_Model');
-	},
-	updateDetail: () => {
-		update_user.run().then(() => {
-			get_all_user.run()
-			closeModal('UD_Model')
-		}).catch((error) => {
-			showAlert(error.message || 'An error occurred while updating user', 'error');
-		})
-	},
+  showDetail: () => {
+    storeValue('selectedUser', TableUserInfo.selectedRow);
+    showModal(UD_Model.name);
+  },
+  
+  updateDetail: async () => {
+    try {
+      if (UDF_FilePicker?.files?.[0]?.data) {
+        await upload_user_icon.run();
+      }
+      
+      await update_user.run();
+      await get_all_user.run();
+      closeModal(UD_Model.name);
+    } catch (error) {
+      showAlert(error.message || 'An error occurred while updating user', 'error');
+    }
+  },
+  
+  create: async () => {
+    try {
+      await create_user.run();
+      await get_all_user.run();
+      closeModal(CreateUserModal.name);
+    } catch (error) {
+      showAlert(error.message || 'An error occurred while creating user', 'error');
+    }
+  }
 }
