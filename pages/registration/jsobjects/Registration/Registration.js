de6958bf77b9865c301144109284a712fe37371c
gcpop-charleshsu <168400762+gcpop-charleshsu@users.noreply.github.com>
2025-09-11 00:04:34 +0800

    add import user (#16)
    Co-authored-by: admin <admin@fliqt.io>

--- pages/registration/jsobjects/Registration/Registration.js
+++ pages/registration/jsobjects/Registration/Registration.js
@@ -1,16 +1,28 @@
 export default {
 	 showDetail: () => {
 		  storeValue('selectedRegister', registration_table.selectedRow)
-			showModal('registration_detail_model');
-			// showAlert(UserTable.selectedRow.creator_name + ' Selected', 'success');
-			// closeModal('UserSelectModal');
+			showModal(registration_detail_model.name);
 	},
 	updateDetail: () => {
 		update_registration_api.run().then(() => {
 			get_registration_api.run()
-			closeModal('registration_detail_model')
+			closeModal(registration_detail_model.name)
 		}).catch((error) => {
-			ErrorMessage.setText(error.message || 'An error occurred while updating registration')
+			// ErrorMessage.setText(error.message || 'An error occurred while updating registration')
+			showAlert(error.message || 'An error occurred while updating registration', 'error');
 		})
 	},
+	importUser: () => {
+		create_user.run().then(() => {
+			get_registration_api.run()
+		}).catch((error) => {
+			showAlert(error.message || 'An error occurred while import user', 'error');
+		})
+		// delete_registration_api.run().then(() => {
+		// 	get_registration_api.run()
+		// }).catch((error) => {
+		// 	showAlert(error.message || 'An error occurred while delete registration', 'error');
+		// })
+		closeModal(registration_detail_model.name)
+	}
 }
