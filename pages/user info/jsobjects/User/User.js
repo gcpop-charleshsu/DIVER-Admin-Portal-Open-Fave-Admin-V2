094d548d463d46f295d83e4a7e987875cbfb2468
admin <admin@fliqt.io>
2025-09-10 07:05:14 +0000

    add user detail update

---
+++ pages/user info/jsobjects/User/User.js
@@ -0,0 +1,14 @@
+export default {
+	 showDetail: () => {
+		  storeValue('selectedUser', TableUserInfo.selectedRow)
+			showModal('UD_Model');
+	},
+	updateDetail: () => {
+		update_user.run(.then(() => {
+			get_all_user.run()
+			closeModal('UD_Model')
+		}).catch((error) => {
+			showAlert(error.message || 'An error occurred while updating user', 'error');
+		})
+	},
+}
